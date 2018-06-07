2ab607fc3c2505065ced96dc50717851b0cb6f64
Leow <lelouchliew93@hotmail.com>
2018-06-07 11:31:11 +0900

    fixed android example project

--- MOLPayReactExampleProject/android/app/src/main/java/com/molpayreactexampleproject/MainActivity.java
+++ MOLPayReactExampleProject/android/app/src/main/java/com/molpayreactexampleproject/MainActivity.java
@@ -1,10 +1,6 @@
 package com.molpayreactexampleproject;
 
 import com.facebook.react.ReactActivity;
-//add these three
-import com.molpayxdk.MOLPayReact;
-import com.molpayxdk.MOLPayReactActivity;
-import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,16 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "MOLPayReactExampleProject";
-    }
-
-    //add activity result in here
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == MOLPayReactActivity.MOLPayReactXDK && resultCode == RESULT_OK){
-            if(MOLPayReact.successCallback != null){
-                MOLPayReact.successCallback.invoke(data.getStringExtra(MOLPayReactActivity.MOLPayTransactionResult));
-            }
-        }
     }
 }
